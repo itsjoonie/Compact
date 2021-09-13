5ef44fcdc1f57a2e15660e92edfeab1267d5d4f8
june <luo.jnny@gmail.com>
2021-09-12 21:59:37 -0700

    banner

---
+++ frontend/src/components/SplashPage/Splash.js
@@ -0,0 +1,26 @@
+import "./Splash.css"
+
+function SplashPage() {
+    return (
+        <div className="Splash-Page">
+
+            {/* banner */}
+                <div className="banner1">
+                    <div className="banner1-content">
+                        <div className="banner1-content-h1">
+                            <h1>Tiny and Cozy Travels</h1>
+                        </div>
+                        <div className="banner1-content-h2">
+                            <h2>Stay Compact</h2>
+                        </div>
+                        <div className="banner1-content-btn">
+                            <button>Explore Now!</button>
+                        </div>
+                    </div>
+                </div>
+        
+        </div>
+    )
+}
+
+export default SplashPage
