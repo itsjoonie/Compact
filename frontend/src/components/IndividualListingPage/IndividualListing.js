7a86f1dce8e12187ced3e2064ac0c3965365901b
june <luo.jnny@gmail.com>
2021-09-30 22:32:20 -0700

    individual listing css is 95% completed

--- frontend/src/components/IndividualListingPage/IndividualListing.js
+++ frontend/src/components/IndividualListingPage/IndividualListing.js
@@ -23,8 +23,12 @@ function IndividualListing(){
     return (
         <div className="individual-listing">
             <div className="individual-listing-container">
-           
-                <h1>Title Goes Here</h1>
+                <div className="individual-listing-header">
+                <h1 className="individual-listing-title">{hosting?.title}</h1>
+                <div>
+                    <p>{hosting?.city}, {hosting?.state}, {hosting?.country}</p>
+                    </div>
+                </div>
                   <div className="images-container">
                     <div className="image-main">
                         <img id="pic1" src={hosting?.Images[0].pic1} alt=""></img>
@@ -40,14 +44,48 @@ function IndividualListing(){
                     </div>
                 </div>
                 <div className="listing-booking-content">
-                    <div>
-                        <div className="listing-star-rating">
-                            star review
+                    <div className="listing-content-left">
+                        <div className="hosted-by">
+                            <h2>Tiny house hosted by {hosting?.User.firstName} {hosting?.User.lastName}</h2>
                         </div>
-                        <div    className="listing-location">
-                            Location here
+                        <div    className="listing-home">
+                            <div>
+                                <p>{hosting?.bed} beds</p>
+                            </div>
+                            <div>
+                                <h3>·</h3>
+                            </div>
+                           
+                            <div>
+                                <p>{hosting?.bathroom} baths</p>
+                                
+                            </div>
+                            <div>
+                                <h3>·</h3>
+                                </div>
+                            
+                            <div>
+                                <p>{hosting?.guest} guests</p>
+                            </div>
                         </div>
-                        <div className="listing-like">Heart
+                        <div className="listing-extra-info">
+                            <div>
+                                <h4><i class="fas fa-home"></i>Entire House</h4><p>The whole tiny house experience to yourself.</p>
+                            </div>
+                            <div>
+                                <h4>
+                                <i class="fas fa-tint"></i>  Enhanced Cleaning
+                                </h4>
+                                <p>This Host committed to Compact's 3-step enhanced cleaning process.</p> 
+                            </div>
+                            <div>
+                                <h4><i class="fas fa-key"></i>Great Check-in Experience</h4>
+                                <p>Easy and contactless check-in.</p>
+                            </div>
+
+                        </div>
+                        <div className="listing-description-info">
+                            <p>{hosting?.description}</p>
                         </div>
                     </div>
                       <div   className="booking-component">
